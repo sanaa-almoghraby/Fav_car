2dd685eb4e0fd4ef549e6a97af82f93b0d3cd8e8
sanaamoghraby <>
2021-04-15 11:57:32 +0000

    type of car

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Fav_car
-**With a colleague Hamza ALmashhrawi**
+**With a colleague Osama Momani**
---
+++ lap05.js
@@ -0,0 +1,19 @@
+alert('Welcome to your favorite car page')
+var carname = prompt('Hello, please enter types of Mercedes cars');
+
+while (carname !== 'van' && carname !== 'suv') {
+  carname = prompt('Please enter the types of car van or suv');
+}
+var userChoose;
+
+if (carname === 'van') {
+  userChoose = '<img src="https://cdn.carbuzz.com/car-thumbnails/300x160/8000/600/8664.jpg">';
+} else if (carname === 'suv') {
+  userChoose = '<img src="https://cdn.carbuzz.com/car-thumbnails/300x160/4000/500/4565.jpg">';
+}
+
+var n_img = prompt('How many image do you want???');
+
+for (var i = 0; i < n_img; i++) {
+  document.write(userChoose);
+}
