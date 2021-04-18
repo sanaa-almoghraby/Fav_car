496361579a06a244a33a42b96dbf6f7557751246
sanaamoghraby <>
2021-04-18 13:02:18 +0000

    lap07

--- lap05.js
+++ lap05.js
@@ -1,11 +1,14 @@
 alert('Welcome to your favorite car page')
 var carname = prompt('Hello, please enter types of Mercedes cars');
-
+var typCar = function(){
 while (carname !== 'van' && carname !== 'suv') {
   carname = prompt('Please enter the types of car van or suv');
 }
-var userChoose;
+}
+typCar();
 
+var userChoose;
+function numImg(){
 if (carname === 'van') {
   userChoose = '<img src="https://cdn.carbuzz.com/car-thumbnails/300x160/8000/600/8664.jpg">';
 } else if (carname === 'suv') {
@@ -17,3 +20,5 @@ var n_img = prompt('How many image do you want???');
 for (var i = 0; i < n_img; i++) {
   document.write(userChoose);
 }
+}
+numImg();
